03a9e5e6af776440a442d97c92d57dc84fc5387b
Ilya Shurumov <ishurumov@gmail.com>
2018-12-26 23:19:37 +0600

    - added missing files

---
+++ DriversGame/Player.cpp
@@ -0,0 +1,45 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Copyright © Inspiration Byte
+// 2009-2019
+//////////////////////////////////////////////////////////////////////////////////
+// Description: Driver Syndicate player data holder
+//////////////////////////////////////////////////////////////////////////////////
+
+#include "Player.h"
+
+CPlayer::CPlayer() : m_ownCar(nullptr)
+{
+}
+
+CPlayer::~CPlayer()
+{
+}
+
+CCar* CPlayer::GetCar()
+{
+	return m_ownCar;
+}
+
+void CPlayer::SetCar(CCar* car)
+{
+	if (m_ownCar)
+		m_ownCar->m_isLocalCar = false;
+
+	m_ownCar = car;
+
+	if(m_ownCar)
+		m_ownCar->m_isLocalCar = true;
+}
+
+bool CPlayer::IsCarAlive()
+{
+	if (m_ownCar)
+		return m_ownCar->IsAlive();
+
+	return false;
+}
+
+void CPlayer::SetControls(const playerControl_t& controls)
+{
+	m_controls = controls;
+}
---
+++ DriversGame/Player.h
@@ -0,0 +1,35 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Copyright © Inspiration Byte
+// 2009-2019
+//////////////////////////////////////////////////////////////////////////////////
+// Description: Driver Syndicate player data holder
+//////////////////////////////////////////////////////////////////////////////////
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "session_base.h"
+
+#include "car.h"
+#include "utils/eqstring.h"
+
+class CPlayer
+{
+public:
+	CPlayer();
+	~CPlayer();
+
+	CCar*				GetCar();
+	void				SetCar(CCar* car);
+
+	bool				IsCarAlive();
+
+	virtual void		SetControls(const playerControl_t& controls);
+
+protected:
+
+	CCar*				m_ownCar;
+	playerControl_t		m_controls;
+};
+
+#endif // PLAYER
