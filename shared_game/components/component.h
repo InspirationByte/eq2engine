09e0c48aec14bf35fbfac049296d5a257186ad19
Ilya Shurumov <ishurumov@gmail.com>
2022-07-10 01:23:51 +0600

    - component changes

--- shared_game/components/component.h
+++ shared_game/components/component.h
@@ -14,13 +14,17 @@ public:
 	ComponentBase(HOST* host) : m_host(host) {}
 	virtual ~ComponentBase() = default;
 
+	virtual const char* GetName() const = 0;
+	virtual int GetNameHash() const = 0;
 protected:
 	HOST*	m_host;
 };
 
 #define DECLARE_COMPONENT(name) \
-	static constexpr const char* GetName() { return name; } \
-	static constexpr int NameHash{ StringToHashConst(name) };
+	static constexpr const char Name[] = name; \
+	static constexpr int NameHash{ StringToHashConst(name) }; \
+	const char* GetName() const override { return name; }\
+	int GetNameHash() const override { return NameHash; }
 
 // hard-linked component instantiator
 #define	ADD_COMPONENT_GETTER(type)	Type* Get<Type>() const { return &m_inst##Type; }
