a8e37993071147d142412cb6ca42c750e120c745
Ilya Shurumov <ishurumov@gmail.com>
2022-10-15 20:48:01 +0600

    - fix EqFunction crash due to storage size

--- public/ds/function.h
+++ public/ds/function.h
@@ -16,34 +16,38 @@ template <int BUFFER_SIZE, typename R, typename ...Args>
 class EqFunction<R(Args...), BUFFER_SIZE>
 {
 public:
-    EqFunction() noexcept : ptr(nullptr), isSmall(false) {};
+    EqFunction() noexcept {};
 
-    EqFunction(std::nullptr_t) noexcept : ptr(nullptr), isSmall(false) {};
+    EqFunction(std::nullptr_t) noexcept {};
 
-    EqFunction(EqFunction const& other) : ptr(nullptr), isSmall(false) {
-        if (!other) {
+    EqFunction(EqFunction const& other)
+    {
+        if (!other)
             return;
-        }
-        if (other.isSmall) {
+
+        if (other.isSmall) 
+        {
             isSmall = true;
             auto c = reinterpret_cast<Concept const*>(&other.buffer);
             c->CopyToBuffer(&buffer);
         }
-        else {
+        else 
+        {
             isSmall = false;
             new (&buffer) std::unique_ptr<Concept>(other.ptr->Copy());
         }
     };
 
-    EqFunction(EqFunction&& other) noexcept : ptr(nullptr), isSmall(false) {
-        if (!other) {
+    EqFunction(EqFunction&& other) noexcept {
+        if (!other)
             return;
-        }
+
         MoveFunction(std::move(other));
     }
 
     template <typename F>
-    EqFunction(F f) {
+    EqFunction(F f)
+    {
         if (sizeof(f) <= BUFFER_SIZE && std::is_nothrow_move_constructible<F>::value) 
         {
             isSmall = true;
@@ -56,33 +60,39 @@ public:
         }
     }
 
-    ~EqFunction() {
+    ~EqFunction()
+    {
         Cleanup();
     }
 
-    EqFunction& operator=(const EqFunction& other) {
+    EqFunction& operator=(const EqFunction& other) 
+    {
         EqFunction tmp(other);
         Swap(tmp);
         return *this;
     }
 
-    EqFunction& operator=(EqFunction&& other) noexcept {
+    EqFunction& operator=(EqFunction&& other) noexcept 
+    {
         Cleanup();
         MoveFunction(std::move(other));
         return *this;
     }
 
-    void Swap(EqFunction& other) noexcept {
+    void Swap(EqFunction& other) noexcept 
+    {
         EqFunction tmp(std::move(other));
         other = std::move(*this);
         *this = std::move(tmp);
     }
 
-    explicit operator bool() const noexcept {
+    explicit operator bool() const noexcept 
+    {
         return isSmall || ptr != nullptr;
     }
 
-    R operator()(Args ... a) const {
+    R operator()(Args ... a) const 
+    {
         if (isSmall) 
         {
             auto c = reinterpret_cast<Concept const*>(&buffer);
@@ -98,11 +108,13 @@ private:
 
     void Cleanup() 
     {
-        if (isSmall) {
+        if (isSmall)
+        {
             auto c = reinterpret_cast<Concept*>(&buffer);
             c->~Concept();
         }
-        else {
+        else 
+        {
             auto c = reinterpret_cast<std::unique_ptr<Concept>*>(&buffer);
             c->~unique_ptr();
         }
@@ -127,7 +139,8 @@ private:
         }
     }
 
-    struct Concept {
+    struct Concept 
+    {
         virtual std::unique_ptr<Concept> Copy() const = 0;
         virtual R Invoke(Args&& ... a) const = 0;
         virtual ~Concept() = default;
@@ -135,26 +148,32 @@ private:
         virtual void MoveToBuffer(void* buf) noexcept = 0;
     };
 
-
     template <typename F>
-    struct Model : Concept {
-        Model(F const& f) : f(f) {}
+    struct Model : Concept 
+    {
+        Model(F const& f) 
+            : f(f) {}
 
-        Model(F&& f) : f(std::move(f)) {}
+        Model(F&& f) 
+            : f(std::move(f)) {}
 
-        std::unique_ptr<Concept> Copy() const override {
+        std::unique_ptr<Concept> Copy() const override
+        {
             return std::make_unique<Model<F>>(f);
         }
 
-        R Invoke(Args&& ... a) const override {
+        R Invoke(Args&& ... a) const override 
+        {
             return f(std::forward<Args>(a)...);
         }
 
-        void CopyToBuffer(void* buf) const override {
+        void CopyToBuffer(void* buf) const override 
+        {
             new (buf) Model<F>(f);
         }
 
-        void MoveToBuffer(void* buf) noexcept override {
+        void MoveToBuffer(void* buf) noexcept override
+        {
             new (buf) Model<F>(std::move(f));
         }
 
@@ -163,11 +182,12 @@ private:
         F f;
     };
 
-    union {
-        mutable std::aligned_storage_t<BUFFER_SIZE, alignof(size_t)> buffer;
+    union
+    {
+        mutable std::aligned_storage_t<BUFFER_SIZE + alignof(size_t), alignof(size_t)> buffer{ 0 };
         std::unique_ptr<Concept> ptr;
     };
 
-    bool isSmall;
+    bool isSmall{ false };
 };
 
