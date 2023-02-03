3cb1faee71adcfada1a649b02126e4b00521b652
Ilya Shurumov <ishurumov@gmail.com>
2023-02-04 01:22:30 +0600

    - try updating script

--- win_install_7zip4powershell.ps1
+++ win_install_7zip4powershell.ps1
@@ -1,4 +1,4 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-PackageProvider -Name NuGet -Scope CurrentUser -MinimumVersion 2.8.5.201 -Force
 Set-PSRepository -Name 'PSGallery' -SourceLocation "https://www.powershellgallery.com/api/v2" -InstallationPolicy Trusted
 Install-Module -Name 7Zip4PowerShell -Force
