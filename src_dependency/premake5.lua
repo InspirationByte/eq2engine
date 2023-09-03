cc037c9c88c77d35a9fceabd426a5a4315342451
SoapyMan <ishurumov@gmail.com>
2023-09-04 00:43:26 +0600

    - remove OOLua from games and repo - remove sol2 from repo

--- src_dependency/premake5.lua
+++ src_dependency/premake5.lua
@@ -18,9 +18,4 @@ include("recast/premake5.lua")
 
 include("bullet2/premake5.lua")
 include("OpenFBX/premake5.lua")
-
-----[[
 include("lua54/premake5.lua")
-include("oolua_premake5.lua")
-include("sol2/premake5.lua")
---]]
