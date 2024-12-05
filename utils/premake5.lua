d98ecd41ddc3c7efbbccba64fad709cf5ce4b174
SoapyMan <ishurumov@gmail.com>
2024-12-05 23:19:55 +0500

    - remove soundsystem_test project

--- utils/premake5.lua
+++ utils/premake5.lua
@@ -107,25 +107,4 @@ project "egfman"
 			"egfman/**.rc"
 		}
 
--- Equilibrium sound system test
-project "soundsystem_test"
-    kind "WindowedApp"
-	unitybuild "on"
-    uses {
-		"corelib", "frameworkLib",
-		"e2Core", 
-		"fontLib", "renderUtilLib", "soundSystemLib", "wxWidgets"
-	}
-    files {
-		"soundsystem_test/*.cpp",
-		"soundsystem_test/*.h"
-	}
-    filter "system:Windows"
-        files {
-			"soundsystem_test/**.rc"
-		}
-		linkoptions {
-			"/SAFESEH:NO", -- Image Has Safe Exception Handers: No. Because of openal-soft
-		}
-		
 end
