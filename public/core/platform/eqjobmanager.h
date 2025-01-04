4c9b7fd2d56b41a3ded8e3eba95027749c1a2eb2
SoapyMan <ishurumov@gmail.com>
2025-01-04 15:05:23 +0500

    - core: add BatchedJob

--- public/core/platform/eqjobmanager.h
+++ public/core/platform/eqjobmanager.h
@@ -89,6 +89,78 @@ public:
 	int			m_count{ 0 };
 };
 
+//--------------------------------------------
+// Batched Job
+
+template<typename ITEM>
+class BatchedJob : public SyncJob
+{
+public:
+	class Worker;
+	using BatchItemList = Array<ITEM>;
+	using BatchItemSpan = ArrayRef<ITEM>;
+
+	BatchedJob(const char* name) : SyncJob(name) { InitSignal(); }
+	void StartJobs(CEqJobManager& jobMng);
+
+private:
+	virtual void GetJobItems(BatchItemList& batchJobItems) = 0;
+	virtual void OnInitWorker(Worker& workerJob) = 0;
+	virtual void Process(ITEM jobItem) = 0;
+
+	class Worker : public IParallelJob
+	{
+	public:
+		Worker(const char* name, BatchedJob& ownerJob) : IParallelJob(name), m_owner(ownerJob) {}
+		void 			Execute() override;
+
+		BatchedJob&		m_owner;
+		BatchItemSpan 	m_batchItems{ nullptr };
+		int				m_threadCount{ 0 };
+		int				m_firstTask{ 0 };
+	};
+
+	BatchItemList 	m_batchItems{ PP_SL };
+	Array<Worker>	m_workerJobs{ PP_SL };
+};
+
+template<typename ITEM>
+void BatchedJob<ITEM>::StartJobs(CEqJobManager& jobMng)
+{
+	GetJobItems(m_batchItems);
+	if (!m_batchItems.numElem())
+	{
+		jobMng.StartJob(this);
+		return;
+	}
+
+	const EqString batchJobName = m_jobName + "Worker";
+	const int tasksPerBatch = m_batchItems.numElem() / jobMng.GetJobThreadsCount();
+	m_workerJobs.assureSizeEmplace(jobMng.GetJobThreadsCount(), batchJobName, *this);
+
+	int numBatchs = 0;
+	for (Worker& workerJob : m_workerJobs)
+	{
+		workerJob.m_batchItems = ArrayRef(m_batchItems.ptr(), m_batchItems.numElem());
+		workerJob.m_firstTask = numBatchs++;
+		workerJob.m_threadCount = jobMng.GetJobThreadsCount();
+		workerJob.InitJob();
+		AddWait(&workerJob);
+		OnInitWorker(workerJob);
+		jobMng.StartJob(&workerJob, false);
+	}
+
+	jobMng.StartJob(this, false);
+	jobMng.Submit(m_workerJobs.numElem() + 1);
+}
+
+template<typename ITEM>
+void BatchedJob<ITEM>::Worker::Execute()
+{
+	for (int i = m_firstTask; i < m_batchItems.numElem(); i += m_threadCount)
+		m_owner.Process(m_batchItems[i]);
+}
+
 //----------------------------------------------------------
 
 class CEqJobManager
