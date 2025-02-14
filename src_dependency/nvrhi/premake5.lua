9eed87569a7a2c95165faeb6f48f5ab486f247cc
SoapyMan <ishurumov@gmail.com>
2025-02-14 14:34:25 +0500

    - remove nvrhi files to make as submodule

--- src_dependency/nvrhi/premake5.lua
+++ src_dependency/nvrhi/premake5.lua
@@ -1,25 +1,44 @@
+local VULKAN_SDK_LOCATION = os.getenv("VULKAN_SDK") or ""
+
 project "nvrhi"
 	properties {
 		"staticlib",
 		"thirdpartylib"
 	}
 	
-	defines { 
-		"NVRHI_WITH_VULKAN"
+	defines {
+		"NOMINMAX",
 		-- no NVRHI_WITH_RTXMU
 		-- no NVRHI_WITH_AFTERMATH
 	}
 	
-	includedirs {
-		"./include"
-	}
-
 	files
 	{
-		"include/**.h",
-		"src/**.h",
-		"src/**.cpp"
+		"NVRHI/include/**.h",
+		"NVRHI/src/**.h",
+		"NVRHI/src/common/**.cpp",
+		"NVRHI/src/validation/**.cpp"
 	}
+	includedirs {
+		"./NVRHI/include"
+	}
+	
+	if false then -- VULKAN_SDK_LOCATION ~= "" then
+		defines {
+			"NOMINMAX",
+			"NVRHI_WITH_VULKAN",
+			"VK_USE_PLATFORM_WIN32_KHR"
+		}
+		files {
+			"NVRHI/src/vulkan/**.cpp"
+		}
+		includedirs {
+			VULKAN_SDK_LOCATION.."/include",
+		}
+		libdirs { 
+			VULKAN_SDK_LOCATION.."./lib",
+		}
+	end
 	
 	filter "configurations:Debug"
 		defines { "NVRHI_WITH_VALIDATION" }
@@ -27,11 +46,15 @@ project "nvrhi"
 	filter "system:windows"
 		defines { 
 			"NVRHI_WITH_DX11",
-			"NVRHI_WITH_DX12",
+			--"NVRHI_WITH_DX12",
+		}
+		files {
+			"NVRHI/src/d3d11/**.cpp",
+			"NVRHI/src/d3d12/**.cpp"
 		}
 		
 usage "nvrhi"
 	includedirs { 
-		"./include"
+		"./NVRHI/include"
 	}
 	links "nvrhi"
