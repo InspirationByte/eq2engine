b8580449086a52ddb9dccc7f58ea814bdb480620
SoapyMan <ishurumov@gmail.com>
2025-08-20 01:07:36 +0500

    - refactor CEffectRenderer

--- shared/render/EffectRender.h
+++ shared/render/EffectRender.h
@@ -10,9 +10,6 @@
 class CParticleBatch;
 using PFXAtlasRef = int;
 
-static constexpr const int MAX_VISIBLE_EFFECTS = 4096;
-
-
 class IEffect
 {
 public:
@@ -28,15 +25,14 @@ public:
 	float			GetDistanceToCamera() const	{ return m_distToView; }
 
 protected:
+	void			InternalInit(const Vector3D& origin, float lifetime, PFXAtlasRef atlasRef);
 
-	void InternalInit(const Vector3D& origin, float lifetime, PFXAtlasRef atlasRef);
+	Vector3D		m_origin{ vec3_zero };
+	PFXAtlasRef		m_atlasRef;
 
-	Vector3D			m_origin{ vec3_zero };
-	PFXAtlasRef			m_atlasRef;
-
-	float				m_lifeTimeRcp{ 0.0f };
-	float				m_lifeTime{ 0.0f };
-	float				m_distToView{ 0.0f };
+	float			m_lifeTimeRcp{ 0.0f };
+	float			m_lifeTime{ 0.0f };
+	float			m_distToView{ 0.0f };
 };
 
 //-------------------------------------------------------------------------------------
@@ -48,6 +44,8 @@ class CEffectRenderer
 {
 	friend class IEffect;
 public:
+	CEffectRenderer();
+
 	void		AddEffect(IEffect* pEffect);
 	void		DrawEffects(float dt);
 
@@ -56,12 +54,9 @@ public:
 	void		SetViewSortPosition(const Vector3D& origin);
 	Vector3D	GetViewSortPosition() const;
 
-protected:
-	void		RemoveEffect(int index);
-
 private:
-	FixedArray<IEffect*, MAX_VISIBLE_EFFECTS>	m_effectList;
-	Vector3D	m_viewPos{ vec3_zero };
+	Array<IEffect*>	m_effectList{ PP_SL };
+	Vector3D		m_viewPos{ vec3_zero };
 };
 
 extern CStaticAutoPtr<CEffectRenderer> g_effectRenderer;
