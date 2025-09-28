52e7dbb0c5158db0a25079598ebfb138e94c3255
SoapyMan <ishurumov@gmail.com>
2025-09-28 16:18:46 +0500

    - fix compilation

--- core/materialsystem1/Renderers/WGPU/WGPULibrary.cpp
+++ core/materialsystem1/Renderers/WGPU/WGPULibrary.cpp
@@ -45,11 +45,11 @@ static const char* s_wgpuDeviceLostReasonStr[] = {
     "FailedCreation",
 };
 
-thread_local WGPUDeviceErrorContext* WGPUDeviceErrorContext::s_currentErrorDeviceContext = nullptr;
+thread_local WGPUDeviceErrorContext* g_currentErrorDeviceContext = nullptr;
 
 static void OnWGPUDeviceError(WGPUDevice const* device, WGPUErrorType type, struct WGPUStringView message, void* userdata1, void* userdata2)
 {
-	WGPUDeviceErrorContext* errorCtx = WGPUDeviceErrorContext::s_currentErrorDeviceContext;
+	WGPUDeviceErrorContext* errorCtx = g_currentErrorDeviceContext;
 	if (errorCtx)
 	{
 		errorCtx->hasError = true;
