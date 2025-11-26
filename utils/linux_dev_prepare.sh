7937a26d0c1eb6b0cbe636abdbb4e58592b125d5
SoapyMan <ishurumov@gmail.com>
2025-11-27 01:28:49 +0500

    - add slang libs downloading to windows & linux dev prepare

--- utils/linux_dev_prepare.sh
+++ utils/linux_dev_prepare.sh
@@ -2,14 +2,18 @@
 set -x
 
 FFMPEG_VER="master-latest-linux64-lgpl-shared"
+SLANG_VER="2025.19.1"
 
 # TODO: openal-soft, SDL2, X11, Wayland, wxGTK packages
 
 wget "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-${FFMPEG_VER}.tar.xz"
 tar -xf ./ffmpeg-${FFMPEG_VER}.tar.xz
-
 mv ./ffmpeg-${FFMPEG_VER}/* "./src_dependency/ffmpeg/"
 rm -rf ./ffmpeg-${FFMPEG_VER}
 rm -f ./ffmpeg-${FFMPEG_VER}.tar.xz
+
+wget "https://github.com/shader-slang/slang/releases/download/v${SLANG_VER}/slang-${SLANG_VER}-linux-x86_64.tar.gz"
+tar -xzf ./slang-${SLANG_VER}-linux-x86_64.tar.gz -C "./src_dependency/slang/"
+rm -f ./slang-${SLANG_VER}-linux-x86_64.tar.gz
 
 echo "Done"
--- utils/windows_dev_prepare.ps1
+++ utils/windows_dev_prepare.ps1
@@ -3,12 +3,14 @@ $openal_ver = '1.23.1'
 $sdl2_ver = '2.30.2'
 $wx_ver = '3.2.1'
 $ffmpeg_ver = 'master-latest-win64-gpl-shared'
+$slang_ver = '2025.19.1'
 
 $windows_openal_url = 'https://github.com/kcat/openal-soft/releases/download/' + $openal_ver + '/openal-soft-' + $openal_ver + '-bin.zip'
 $windows_sdl2_url = 'https://github.com/libsdl-org/SDL/releases/download/release-' + $sdl2_ver + '/SDL2-devel-' + $sdl2_ver + '-VC.zip'
 $windows_wx_hdrs_url = 'https://github.com/wxWidgets/wxWidgets/releases/download/v' +$wx_ver+ '/wxWidgets-' +$wx_ver+ '-headers.7z'
 $windows_wx_libs_url = 'https://github.com/wxWidgets/wxWidgets/releases/download/v' +$wx_ver+ '/wxMSW-' +$wx_ver+ '_vc14x_x64_Dev.7z'
 $windows_ffmpeg_url = 'https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-' +$ffmpeg_ver+ '.zip'
+$windows_slang_url = 'https://github.com/shader-slang/slang/releases/download/v' +$slang_ver+ '/slang-' +$slang_ver+ '-windows-x86_64.zip'
 
 $project_folder = '.\\'
 $dependency_folder = $project_folder + '\\src_dependency'
@@ -19,12 +21,14 @@ Invoke-WebRequest -Uri $windows_openal_url -OutFile OPENAL.zip
 Invoke-WebRequest -Uri $windows_wx_hdrs_url -OutFile WX_HDRS.7z
 Invoke-WebRequest -Uri $windows_wx_libs_url -OutFile WX_LIBS.7z
 Invoke-WebRequest -Uri $windows_ffmpeg_url -OutFile FFMPEG.zip
+Invoke-WebRequest -Uri $windows_slang_url -OutFile SLANG.zip
 
 Expand-Archive SDL2.zip -DestinationPath $dependency_folder
 Expand-Archive OPENAL.zip -DestinationPath $dependency_folder
 Expand-7Zip -ArchiveFileName WX_HDRS.7z -TargetPath ($dependency_folder + '\\wxWidgets')
 Expand-7Zip -ArchiveFileName WX_LIBS.7z -TargetPath ($dependency_folder + '\\wxWidgets')
 Expand-Archive FFMPEG.zip -DestinationPath $dependency_folder
+Expand-Archive SLANG.zip -DestinationPath ($dependency_folder + '\\slang')
 
 # Generate project files
 $windows_openal_dir = ('.\\src_dependency\\openal-soft-' + $openal_ver + '-bin')
@@ -44,6 +48,7 @@ Remove-Item OPENAL.zip
 Remove-Item WX_HDRS.7z
 Remove-Item WX_LIBS.7z
 Remove-Item FFMPEG.zip
+Remove-Item SLANG.zip
 
 Set-Location -Path $project_folder
 
