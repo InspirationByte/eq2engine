3670483ced69240de49b66f43293395960af280d
SoapyMan <ishurumov@gmail.com>
2026-08-03 22:50:45 +0300

    - ds: mempool to allow not yet defined types in headers

--- framework/ds/mempool.h
+++ framework/ds/mempool.h
@@ -6,13 +6,17 @@ template <typename T, int CHUNK_ITEMS = 1024>
 class MemoryPool
 {
 public:
-	MemoryPool(const MemoryPool& memoryPool) = delete;
-	MemoryPool operator =(const MemoryPool& memoryPool) = delete;
+	virtual ~MemoryPool();
 
 	MemoryPool(PPSourceLine sl)
 		: m_sl(sl)
 	{
 	}
+
+	MemoryPool(const MemoryPool& memoryPool) = delete;
+	MemoryPool operator =(const MemoryPool& memoryPool) = delete;
+
+
 
 	MemoryPool(MemoryPool&& other) noexcept
 		: m_sl(other.m_sl)
@@ -35,61 +39,9 @@ public:
 		return *this;
 	}
 
-	virtual ~MemoryPool()
-	{
-		Buffer* buffer = m_firstBuffer;
-		while (buffer)
-		{
-			Buffer* del = buffer;
-			buffer = buffer->next;
-			delete del;
-		}
-	}
-
-	void clear()
-	{
-		Buffer* buffer = m_firstBuffer;
-		while (buffer)
-		{
-			Buffer* del = buffer;
-			buffer = buffer->next;
-			delete del;
-		}
-		m_firstFreeBlock = nullptr;
-		m_firstBuffer = nullptr;
-		m_bufferedBlocks = CHUNK_ITEMS;
-	}
-
-	T* allocate()
-	{
-		if (m_firstFreeBlock)
-		{
-			Block* block = m_firstFreeBlock;
-			m_firstFreeBlock = block->next;
-			return reinterpret_cast<T*>(block);
-		}
-
-		if (m_bufferedBlocks >= CHUNK_ITEMS)
-		{
-			m_firstBuffer = PPNewSL(m_sl) Buffer(m_firstBuffer);
-			m_bufferedBlocks = 0;
-		}
-
-		T* ptr = m_firstBuffer->getBlock(m_bufferedBlocks++);
-		return ptr;
-	}
-
-	void deallocate(T* pointer)
-	{
-		if (!pointer)
-			return;
-
-		dbgValidatePtr(pointer);
-
-		Block* block = reinterpret_cast<Block*>(pointer);
-		block->next = m_firstFreeBlock;
-		m_firstFreeBlock = block;
-	}
+	void	clear();
+	T*		allocate();
+	void	deallocate(T* pointer);
 
 	const PPSourceLine getSL() const { return m_sl; }
 
@@ -118,16 +70,7 @@ private:
 		Block*			next{ nullptr };
 	};
 
-	static constexpr size_t BLOCK_SIZE = max(sizeof(T), sizeof(Block));
-
-	struct Buffer
-	{
-		uint8			data[BLOCK_SIZE * CHUNK_ITEMS];
-		Buffer* const	next{ nullptr };
-
-		Buffer(Buffer* next) : next(next) {}
-		T*				getBlock(int index) { return reinterpret_cast<T*>(&data[BLOCK_SIZE * index]); }
-	};
+	struct Buffer;
 
 	PPSourceLine 	m_sl;
 	Block*			m_firstFreeBlock{ nullptr };
@@ -135,6 +78,81 @@ private:
 	int				m_bufferedBlocks{ CHUNK_ITEMS };
 };
 
+//--------------------------------------------------------------
+
+template <typename T, int CHUNK_ITEMS>
+struct MemoryPool<T, CHUNK_ITEMS>::Buffer
+{
+	static constexpr size_t BLOCK_SIZE = max(sizeof(T), sizeof(Block));
+
+	uint8			data[BLOCK_SIZE * CHUNK_ITEMS];
+	Buffer* const	next{ nullptr };
+
+	Buffer(Buffer* next) : next(next) {}
+	T* getBlock(int index) { return reinterpret_cast<T*>(&data[BLOCK_SIZE * index]); }
+};
+
+template <typename T, int CHUNK_ITEMS>
+MemoryPool<T, CHUNK_ITEMS>::~MemoryPool()
+{
+	Buffer* buffer = m_firstBuffer;
+	while (buffer)
+	{
+		Buffer* del = buffer;
+		buffer = buffer->next;
+		delete del;
+	}
+}
+
+template <typename T, int CHUNK_ITEMS>
+void MemoryPool<T, CHUNK_ITEMS>::clear()
+{
+	Buffer* buffer = m_firstBuffer;
+	while (buffer)
+	{
+		Buffer* del = buffer;
+		buffer = buffer->next;
+		delete del;
+	}
+	m_firstFreeBlock = nullptr;
+	m_firstBuffer = nullptr;
+	m_bufferedBlocks = CHUNK_ITEMS;
+}
+
+template <typename T, int CHUNK_ITEMS>
+T* MemoryPool<T, CHUNK_ITEMS>::allocate()
+{
+	if (m_firstFreeBlock)
+	{
+		Block* block = m_firstFreeBlock;
+		m_firstFreeBlock = block->next;
+		return reinterpret_cast<T*>(block);
+	}
+
+	if (m_bufferedBlocks >= CHUNK_ITEMS)
+	{
+		m_firstBuffer = PPNewSL(m_sl) Buffer(m_firstBuffer);
+		m_bufferedBlocks = 0;
+	}
+
+	T* ptr = m_firstBuffer->getBlock(m_bufferedBlocks++);
+	return ptr;
+}
+
+template <typename T, int CHUNK_ITEMS>
+void MemoryPool<T, CHUNK_ITEMS>::deallocate(T* pointer)
+{
+	if (!pointer)
+		return;
+
+	dbgValidatePtr(pointer);
+
+	Block* block = reinterpret_cast<Block*>(pointer);
+	block->next = m_firstFreeBlock;
+	m_firstFreeBlock = block;
+}
+
+//--------------------------------------------------------------
 
 // std-compatible allocator
 template <class T, int CHUNK_ITEMS = 1024>
